733129f13d704d335f4b0872b16650ab6cf0d0ce
Bhupesh <bhupesh100@gmail.com>
2020-11-08 19:05:03 +0000

    Wrapping Database Data in Program Objects

--- app.rb
+++ app.rb
@@ -13,10 +13,11 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/bookmarks' do
-    Bookmark.create(url: params[:url])
+    Bookmark.create(url: params[:url], title: params[:title])
     redirect '/bookmarks'
   end
 
+partition { |e|  }
   run! if app_file == $0
 
 end
